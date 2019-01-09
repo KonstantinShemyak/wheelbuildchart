2c70e5ccb6df75586555a85082388bf21504a5ba
plugz <david.garyga@gmail.com>
2019-01-09 18:07:51 -0500

    Update all on any change

--- wheelchart.js
+++ wheelchart.js
@@ -16,14 +16,17 @@ $(document).ready(function() {
 	// Fill dropdown lists with values
 	var $spokesList = $('#nSpokes');
 	fillSelectFromArray($spokesList, config.SUPPORTED_SPOKE_COUNTS, config.DEFAULT_SPOKES);
+	$spokesList.on('change', initValuesTable);
 
 	var knownSpokeThickness = TensionTable.getKnownSpokeThickness();
 
 	var $spokeThicknessList = $('#spokeThickness');
 	fillSelectFromArray($spokeThicknessList, knownSpokeThickness, config.DEFAULT_SPOKE_THICKNESS);
+	$spokeThicknessList.on('change', updateCalculations);
 
 	var $spokeThicknessNDSList = $('#spokeThicknessNDS');
 	fillSelectFromArray($spokeThicknessNDSList, knownSpokeThickness, config.DEFAULT_SPOKE_THICKNESS);
+	$spokeThicknessNDSList.on('change', updateCalculations);
 	
 	// Tension value from the table, for the selected spoke thickness
 	function tensionFunction(r) {
@@ -33,14 +36,21 @@ $(document).ready(function() {
 		return TensionTable.parkToolTension($spokeThicknessNDSList.val(), r);
 	};
 
-	// Emulate click, which will draw everything
-	$('#startButton').on('click', function() {
-		initValuesTable($spokesList.val());
-	}).click();
+	// Fill readings array up to the max possible number of spokes
+	var readings = [];
+	for (var i = 1; i <= config.SUPPORTED_SPOKE_COUNTS[config.SUPPORTED_SPOKE_COUNTS.length - 1]; ++i) {
+		if (i % 2 == 1)
+			readings.push(config.INITIAL_READING_DRIVE_SIDE);
+		else
+			readings.push(config.INITIAL_READING_NON_DRIVE_SIDE);
+	}
+
+	initValuesTable();
 
 	// Functions
 
-	function initValuesTable(nSpokes) {
+	function initValuesTable() {
+		var nSpokes = Number($spokesList.val());
 		$('[role="valueRow"]').remove();
 
 		// Vertical offset between left and right columns:
@@ -56,13 +66,13 @@ $(document).ready(function() {
 
 			var $input = $('<input type="text" id="reading' + i + '"/>')
 			.addClass("readInput")
-			.on('change', handleUserInput(i, nSpokes));
+			.on('change', handleUserInput(i));
 
 			if (i % 2 != 0) {
 				$('<td class="driveSideColor" rowspan="2"/>')
 				.text('#' + i + ':').appendTo($row);
 				$('<td class="driveSideColor" rowspan="2"/>')
-				.append($input.val(config.INITIAL_READING_DRIVE_SIDE))
+				.append($input.val(readings[i - 1]))
 				.appendTo($row);
 				$('<td class="driveSideColor" rowspan="2"/>')
 				.attr("id", "tension" + i).attr("side", "drive")
@@ -72,7 +82,7 @@ $(document).ready(function() {
 				$('<td class="nonDriveSideColor" rowspan="2"/>')
 				.text('#' + i + ':').appendTo($row);
 				$('<td class="nonDriveSideColor" rowspan="2"/>')
-				.append($input.val(config.INITIAL_READING_NON_DRIVE_SIDE))
+				.append($input.val(readings[i - 1]))
 				.appendTo($row);
 				$('<td class="nonDriveSideColor" rowspan="2"/>')
 				.attr("id", "tension" + i)
@@ -84,19 +94,18 @@ $(document).ready(function() {
 		// End the vertical offset with a left-side one-line cell
 		$('<tr role="valueRow"><td class="driveSideColor" colspan="3"/></tr>').insertBefore($('#average'));
 
-		$('#avgDriveReading').text(config.INITIAL_READING_DRIVE_SIDE);
-		$('#avgDriveTension').text(tensionFunction(config.INITIAL_READING_DRIVE_SIDE));
-		$('#avgNondriveReading').text(config.INITIAL_READING_NON_DRIVE_SIDE);
-		$('#avgNondriveTension').text(tensionFunctionNDS(config.INITIAL_READING_NON_DRIVE_SIDE));
-		
 		$('#reading1').focus().select();
+
 		tensionChart.init('#radarChart', nSpokes,
 				tensionFunction(config.INITIAL_READING_DRIVE_SIDE),
 				tensionFunction(config.INITIAL_READING_NON_DRIVE_SIDE));
+
+		updateCalculations();
 	}
 
-	function handleUserInput(targetSpoke, nSpokes) {
+	function handleUserInput(targetSpoke) {
 		return function() {
+			var nSpokes = Number($spokesList.val());
 			var userInput = $('#reading' + targetSpoke).val();
 			/* Restrict user input just for safety: */			
 			if (!config.ALLOWED_INPUT_REGEXP.test(userInput)) {
@@ -105,37 +114,49 @@ $(document).ready(function() {
 				return;
 			}
 				
-			/* Calculate the new value: */
-			var newTension;
-			if (targetSpoke % 2 == 1)
-				newTension = tensionFunction(userInput);
-			else
-				newTension = tensionFunctionNDS(userInput);
-			$('#tension' + targetSpoke).text(round2(newTension));
-			
-			/* Update the averages: */
-			var selector = targetSpoke % 2 == 1 ? "avgDrive" : "avgNondrive";
-			var sumReading = 0, sumValue = 0;
-			
-			var startingSpoke = targetSpoke % 2 == 1 ? 1 : 2;
-			for (var i = startingSpoke; i <= nSpokes; i += 2) {
-				sumReading += parseFloat($('#reading' + i).val());
-				sumValue += parseFloat($('#tension' + i).text());
-			}
-
-			$('#' + selector + 'Reading').text(round2(sumReading / nSpokes * 2));
-			$('#' + selector + 'Tension').text(round2(sumValue / nSpokes * 2));
+			readings[targetSpoke - 1] = parseFloat(userInput);
+			updateCalculations();
 
 			/* Move the focus: e.g. for 32-spoke wheel, 31 -> 1, 32 -> 2 */
 			var nextSameSideSpoke = targetSpoke + 2;
-			if (nextSameSideSpoke == Number(nSpokes) + 1)
+			if (nextSameSideSpoke == nSpokes + 1)
 				nextSameSideSpoke = 1;
-			else if (nextSameSideSpoke == Number(nSpokes) + 2)
+			else if (nextSameSideSpoke == nSpokes + 2)
 				nextSameSideSpoke = 2;
 			$('#reading' + nextSameSideSpoke).focus().select();
-			
-			tensionChart.update(targetSpoke, newTension);
 		};
+	}
+
+	function updateCalculations() {
+		var nSpokes = Number($spokesList.val());
+
+		var tensions = [];
+		var sumReadings = [0, 0]; // [nds, ds]
+		var sumTensions = [0, 0]; // [nds, ds]
+
+		for (var i = 1; i <= nSpokes; ++i) {
+			if (i % 2 == 1)
+				tensions[i - 1] = tensionFunction(readings[i - 1]);
+			else
+				tensions[i - 1] = tensionFunctionNDS(readings[i - 1]);
+			sumReadings[i % 2] += readings[i - 1];
+			sumTensions[i % 2] += tensions[i - 1];
+		}
+
+		var avgReadings = [sumReadings[0] / (nSpokes / 2), sumReadings[1] / (nSpokes / 2)]; // [nds, ds]
+		var avgTensions = [sumTensions[0] / (nSpokes / 2), sumTensions[1] / (nSpokes / 2)]; // [nds, ds]
+
+		$('#avgDriveReading').text(round2(avgReadings[1]));
+		$('#avgDriveTension').text(round2(avgTensions[1]));
+		$('#avgNondriveReading').text(round2(avgReadings[0]));
+		$('#avgNondriveTension').text(round2(avgTensions[0]));
+
+		// update each spoke
+		for (var i = 1; i <= nSpokes; ++i) {
+			$('#tension' + i).text(round2(tensions[i - 1]));
+		}
+
+		tensionChart.updateAll(tensions);
 	}
 	
 	// Utilities

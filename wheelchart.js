c0cac243d770898c7df78e6f34689f2eb138a6ca
plugz <david.garyga@gmail.com>
2019-01-09 18:03:05 -0500

    Update all on any change

--- wheelchart.js
+++ wheelchart.js
@@ -18,17 +18,21 @@ $(document).ready(function() {
 	// Fill dropdown lists with values
 	var $spokesList = $('#nSpokes');
 	fillSelectFromArray($spokesList, config.SUPPORTED_SPOKE_COUNTS, config.DEFAULT_SPOKES);
+	$spokesList.on('change', initValuesTable);
 
 	var knownSpokeThickness = TensionTable.getKnownSpokeThickness();
 
 	var $spokeThicknessList = $('#spokeThickness');
 	fillSelectFromArray($spokeThicknessList, knownSpokeThickness, config.DEFAULT_SPOKE_THICKNESS);
+	$spokeThicknessList.on('change', updateCalculations);
 
 	var $spokeThicknessNDSList = $('#spokeThicknessNDS');
 	fillSelectFromArray($spokeThicknessNDSList, knownSpokeThickness, config.DEFAULT_SPOKE_THICKNESS);
+	$spokeThicknessNDSList.on('change', updateCalculations);
 	
 	var $toleranceInput = $('#toleranceInput');
 	fillSelectFromArray($toleranceInput, config.SUPPORTED_TOLERANCES, config.DEFAULT_TOLERANCE);
+	$toleranceInput.on('change', updateCalculations);
 
 	// Tension value from the table, for the selected spoke thickness
 	function tensionFunction(r) {
@@ -38,14 +42,21 @@ $(document).ready(function() {
 		return TensionTable.parkToolTension($spokeThicknessNDSList.val(), r);
 	};
 
-	// Emulate click, which will draw everything
-	$('#startButton').on('click', function() {
-		initValuesTable($spokesList.val());
-	}).click();
+	// Fill readings array up to the max possible number of spokes
+	var readings = [];
+	for (var i = 1; i <= config.SUPPORTED_SPOKE_COUNTS[config.SUPPORTED_SPOKE_COUNTS.length - 1]; ++i) {
+		if (i % 2 == 1)
+			readings.push(config.INITIAL_READING_DRIVE_SIDE);
+		else
+			readings.push(config.INITIAL_READING_NON_DRIVE_SIDE);
+	}
+
+	initValuesTable();
 
 	// Functions
 
-	function initValuesTable(nSpokes) {
+	function initValuesTable() {
+		var nSpokes = Number($spokesList.val());
 		$('[role="valueRow"]').remove();
 
 		for (var i = 1; i <= nSpokes; i++) {
@@ -53,13 +64,13 @@ $(document).ready(function() {
 
 			var $input = $('<input type="text" id="reading' + i + '"/>')
 			.addClass("readInput")
-			.on('change', handleUserInput(i, nSpokes));
+			.on('change', handleUserInput(i));
 
 			if (i % 2 != 0) {
 				$('<td class="driveSideColor"/>')
 				.text('#' + i + ':').appendTo($row);
 				$('<td class="driveSideColor"/>')
-				.append($input.val(config.INITIAL_READING_DRIVE_SIDE))
+				.append($input.val(readings[i - 1]))
 				.appendTo($row);
 				$('<td class="driveSideColor"/>')
 				.attr("id", "tension" + i).attr("side", "drive")
@@ -71,7 +82,7 @@ $(document).ready(function() {
 				$('<td class="nonDriveSideColor"/>')
 				.text('#' + i + ':').appendTo($row);
 				$('<td class="nonDriveSideColor"/>')
-				.append($input.val(config.INITIAL_READING_NON_DRIVE_SIDE))
+				.append($input.val(readings[i - 1]))
 				.appendTo($row);
 				$('<td class="nonDriveSideColor"/>')
 				.attr("id", "tension" + i)
@@ -80,19 +91,18 @@ $(document).ready(function() {
 			}
 			$row.insertBefore($('#average'));
 		}
-		$('#avgDriveReading').text(config.INITIAL_READING_DRIVE_SIDE);
-		$('#avgDriveTension').text(tensionFunction(config.INITIAL_READING_DRIVE_SIDE));
-		$('#avgNondriveReading').text(config.INITIAL_READING_NON_DRIVE_SIDE);
-		$('#avgNondriveTension').text(tensionFunctionNDS(config.INITIAL_READING_NON_DRIVE_SIDE));
-		
 		$('#reading1').focus().select();
+
 		tensionChart.init('#radarChart', nSpokes,
 				tensionFunction(config.INITIAL_READING_DRIVE_SIDE),
 				tensionFunction(config.INITIAL_READING_NON_DRIVE_SIDE));
+
+		updateCalculations();
 	}
 
-	function handleUserInput(targetSpoke, nSpokes) {
+	function handleUserInput(targetSpoke) {
 		return function() {
+			var nSpokes = Number($spokesList.val());
 			var userInput = $('#reading' + targetSpoke).val();
 			/* Restrict user input just for safety: */			
 			if (!config.ALLOWED_INPUT_REGEXP.test(userInput)) {
@@ -101,50 +111,56 @@ $(document).ready(function() {
 				return;
 			}
 				
-			/* Calculate the new value: */
-			var newTension;
-			if (targetSpoke % 2 == 1)
-				newTension = tensionFunction(userInput);
-			else
-				newTension = tensionFunctionNDS(userInput);
-			
-			/* Update the averages: */
-			var selector = targetSpoke % 2 == 1 ? "avgDrive" : "avgNondrive";
-			var sumReading = 0, sumTension = 0;
-			
-			var startingSpoke = targetSpoke % 2 == 1 ? 1 : 2;
-			for (var i = startingSpoke; i <= nSpokes; i += 2) {
-				sumReading += parseFloat($('#reading' + i).val());
-				if (i != targetSpoke)
-					sumTension += parseFloat($('#tension' + i).text());
-				else
-					sumTension += newTension;
-			}
-
-			var avgReading =  sumReading / (nSpokes / 2);
-			var avgTension =  sumTension / (nSpokes / 2);
-
-			$('#' + selector + 'Reading').text(round2(avgReading));
-			$('#' + selector + 'Tension').text(round2(avgTension));
-
-			var minTension = avgTension * ((100 - $toleranceInput.val()) / 100);
-			var maxTension = avgTension * ((100 + $toleranceInput.val()) / 100);
-
-			if (newTension > minTension && newTension < maxTension)
-				$('#tension' + targetSpoke).text(round2(newTension));
-			else
-				$('#tension' + targetSpoke).html(round2(newTension) + " &#9888;");
+			readings[targetSpoke - 1] = parseFloat(userInput);
+			updateCalculations();
 
 			/* Move the focus: e.g. for 32-spoke wheel, 31 -> 1, 32 -> 2 */
 			var nextSameSideSpoke = targetSpoke + 2;
-			if (nextSameSideSpoke == Number(nSpokes) + 1)
+			if (nextSameSideSpoke == nSpokes + 1)
 				nextSameSideSpoke = 1;
-			else if (nextSameSideSpoke == Number(nSpokes) + 2)
+			else if (nextSameSideSpoke == nSpokes + 2)
 				nextSameSideSpoke = 2;
 			$('#reading' + nextSameSideSpoke).focus().select();
-			
-			tensionChart.update(targetSpoke, newTension);
 		};
+	}
+
+	function updateCalculations() {
+		var nSpokes = Number($spokesList.val());
+
+		var tensions = [];
+		var sumReadings = [0, 0]; // [nds, ds]
+		var sumTensions = [0, 0]; // [nds, ds]
+
+		for (var i = 1; i <= nSpokes; ++i) {
+			if (i % 2 == 1)
+				tensions[i - 1] = tensionFunction(readings[i - 1]);
+			else
+				tensions[i - 1] = tensionFunctionNDS(readings[i - 1]);
+			sumReadings[i % 2] += readings[i - 1];
+			sumTensions[i % 2] += tensions[i - 1];
+		}
+
+		var avgReadings = [sumReadings[0] / (nSpokes / 2), sumReadings[1] / (nSpokes / 2)]; // [nds, ds]
+		var avgTensions = [sumTensions[0] / (nSpokes / 2), sumTensions[1] / (nSpokes / 2)]; // [nds, ds]
+
+		$('#avgDriveReading').text(round2(avgReadings[1]));
+		$('#avgDriveTension').text(round2(avgTensions[1]));
+		$('#avgNondriveReading').text(round2(avgReadings[0]));
+		$('#avgNondriveTension').text(round2(avgTensions[0]));
+
+		var tolerance = parseFloat($toleranceInput.val());
+		var minTensions = [avgTensions[0] * ((100 - tolerance) / 100), avgTensions[1] * ((100 - tolerance) / 100)];
+		var maxTensions = [avgTensions[0] * ((100 + tolerance) / 100), avgTensions[1] * ((100 + tolerance) / 100)];
+
+		// update each spoke
+		for (var i = 1; i <= nSpokes; ++i) {
+			if (tensions[i - 1] > minTensions[i % 2] && tensions[i - 1] < maxTensions[i % 2])
+				$('#tension' + i).text(round2(tensions[i - 1]));
+			else
+				$('#tension' + i).html(round2(tensions[i - 1]) + " &#9888;");
+		}
+
+		tensionChart.updateAll(tensions);
 	}
 	
 	// Utilities

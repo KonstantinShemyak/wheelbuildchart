300083caffb238a4126053a65383b34585eba8f8
Konstantin Shemyak <konstantin@shemyak.com>
2026-01-25 20:30:27 +0200

    chore: add Typescript and Vite
    Start of a refactoring.
    Type violations are not yet all handled.

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+// Global type declarations for legacy code
+// These are loaded via script tags in index.html
+
+declare const d3: any;
+declare const $: JQueryStatic;
+declare const jQuery: JQueryStatic;
